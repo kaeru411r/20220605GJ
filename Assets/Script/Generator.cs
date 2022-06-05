5670bed24fc4f6876a8fc737b9492a969b822540
_kazuki <kazkazm.k@icloud.com>
2022-06-05 15:03:43 +0900

    ランダムな位置に雨を降らせるscript

--- Assets/Script/Generator.cs
+++ Assets/Script/Generator.cs
@@ -4,16 +4,29 @@ using UnityEngine;
 
 public class Generator : MonoBehaviour
 {
-    public GameObject[] fall;
+    [SerializeField] GameObject fall;
+    [SerializeField] float rightArea;
+    [SerializeField] float leftArea;
+    private bool isPlay;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        isPlay = false;
+    }
+    public void GameStart()
+    {
+        isPlay = true;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(isPlay)
+        {
+            float x = Random.Range(-8.0f,8.0f);
+            float y = 5f;
+        }
     }
 }
