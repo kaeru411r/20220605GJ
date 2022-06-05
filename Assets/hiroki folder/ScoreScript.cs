721cccf00a710b5e8ed1174ca47712e081301e5c
Hiroki_Tanimura <i.am.a.hero.1231@gmail.com>
2022-06-05 14:37:03 +0900

    スコアとタイマーの表示

--- Assets/hiroki folder/ScoreScript.cs
+++ Assets/hiroki folder/ScoreScript.cs
@@ -1,23 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreScript : MonoBehaviour
 {
     int m_score;
+    int m_flowerScore = 0;
+    Text m_scoreText=default;
+    
+
     // Start is called before the first frame update
     public void AddScore(int score)
     {
         m_score += score;
     }
+    public void AddFlowerScore()
+    {
+        m_flowerScore++; 
+    }
     void Start()
     {
-        
+        m_scoreText = GetComponent<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        m_scoreText.text = m_score.ToString();
     }
 }
