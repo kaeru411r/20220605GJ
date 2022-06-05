bf1fc147f2a55648f20cc7af2e254bfecf9160ff
kaeru411r <ike269fr@gmail.com>
2022-06-05 13:13:24 +0900

    GameManagerの作成開始

---
+++ Assets/Ikeda/GameManeger.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManeger
+{
+    static private GameManeger _instance = new GameManeger();
+    private GameManeger() { }
+    static public GameManeger Instance => _instance;
+
+    public void GameStart()
+    {
+
+    }
+
+    public void GameRestart()
+    {
+
+    }
+
+    public void GameClear()
+    {
+
+    }
+
+    public void GameOver()
+    {
+
+    }
+
+    public void CloseApplication()
+    {
+
+    }
+
+}
